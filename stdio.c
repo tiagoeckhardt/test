8085781556fd072b6653a6d1c1207199f0463350
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2019-12-09 11:59:06 +0000

    Update stdio.c

--- stdio.c
+++ stdio.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-	int num;
-        printf("Qual a maior floresta do mundo:\n");
-        printf("1 – A floresta da Amazónia\n");
-        printf("2 – A floresta do Congo\n");
-        printf("3 – A floresta de Taiga\n");
-        printf("4 – A floresta de Sinharaja\n");
-        printf("Selecione a opção correcta 1, 2, 3 ou 4.\n");
-        scanf("%d", &num);
-        switch (num) {
-        case 1: printf("Certo!\n"); break;
-        case 2: printf("A floresta tropical do Congo é a segunda maior floresta tropical do mundo, cobrindo 1.800.000 km².\n"); break;
-        case 3: printf("A floresta de Taiga tem a maior comunidade biótica.\n"); break;
-        case 4: printf("A floresta de Sinharaja tem 21 km.\n"); break;
-        default: printf("Número incorreto!\n");
-        }
+int main()
+{
+    struct entry {
+        char actor[32];
+        int year;
+        char title[32];
+    };
+    struct entry bond2;
+    struct entry bond3;
+    FILE *a007;
+
+    strcpy(bond2.actor,"Roger Moore");
+    bond2.year = 1973;
+    strcpy(bond2.title,"Live and Let Die");
+    strcpy(bond3.actor,"Pierce Brosnan");
+    bond3.year = 1995;
+    strcpy(bond3.title,"GoldenEye");
+
+    a007 = fopen("bond.db","a");
+    if(!a007)
+    {
+        puts("SPECTRE wins!");
+        exit(1);
+    }
+    fwrite(&bond2,sizeof(struct entry),1,a007);
+    fwrite(&bond3,sizeof(struct entry),1,a007);
+    fclose(a007);
+    puts("Records written");
+
+    return(0);
 }
